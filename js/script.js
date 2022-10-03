34dce8495282eaec6ecc16ba15eaaaf087327a80
MonicaHillman <mmhillman1@ucs.br>
2022-10-03 16:07:03 -0300

    pequeno ajuste

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ehUmCPF from "./valida-cpf.js";
 const camposDoFormulario = document.querySelectorAll('[required]')
 
 camposDoFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => verificaCampo());
+    campo.addEventListener("blur", () => verificaCampo(campo));
 })
 
 function verificaCampo(campo) {
