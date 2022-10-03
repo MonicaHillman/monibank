f9fe062646f817d030170244dd589b8543b6a2d0
MonicaHillman <mmhillman1@ucs.br>
2022-10-03 15:03:49 -0300

    começando com o js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import ehUmCPF from "./valida-cpf.js";
+const camposDoFormulario = document.querySelectorAll('[required]')
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo());
+})
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+}
---
+++ js/valida-cpf.js
@@ -0,0 +1,23 @@
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    validaNumerosRepetidos(cpf);
+
+    console.log(validaNumerosRepetidos(cpf));
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numerosRepetidos.includes(cpf)
+}
